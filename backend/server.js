163e15b98e10050f5426ebf74b5fc1b8cab3900a
nathang0147 <nganhthang147@gmail.com>
2024-03-25 15:33:41 +0700

    mod: Fix mongoDB connection

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./src/routes');
+
+//Create express
+const app = express();
+
+//Use the express-static middleware
+app.use(express.json()); // for parsing application/json for post/patch request to access to body(data) of request
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+//routes
+routes(app);
+
+//Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+      app.listen(process.env.PORT, () => {
+        console.log(`Example app listening on port http://localhost:${process.env.PORT}`);
+      });
+    })
+    .catch((err)=>{
+        console.error(`Error connecting to the database. \n${err}`);
+    });
+
