e32c5ed218ad9a3ce88f0f95d136cfb3d9d9f877
nathang0147 <nganhthang147@gmail.com>
2024-03-25 15:33:20 +0700

    Feat: authentication handle API function

---
+++ backend/src/controller/authController.js
@@ -0,0 +1,39 @@
+const User = require('../models/userModel');
+
+class AuthController {
+    async signup_get(req, res) {
+        res.render('signup');
+    }
+
+    async signup_post(req, res) {
+        const {fullName, userName, password, confirmPassword, gender } = req.body;
+        try{
+         const user = await User.signup(fullName, userName, password, confirmPassword, gender);
+
+        console.log(user);
+        res.status(201).json({
+            _id: user._id,
+            fullName: user.fullName,
+            profilePicture: user.profilePicture,
+        })
+        }catch (e){
+            console.log(e.message
+            )
+            res.status(400).json({error: e.message});
+        }
+    }
+
+    async login_get(req, res) {
+        res.render('login');
+    }
+
+    async login_post(req, res) {
+        res.send('user login');
+    }
+
+    async logout(req, res) {
+        res.send('user logout');
+    }
+}
+
+module.exports = new AuthController();
