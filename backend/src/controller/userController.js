0a7b5fa89d7acc14f8980f18a7c199ba97fecbbd
nathang0147 <nganhthang147@gmail.com>
2024-04-01 17:36:53 +0700

    feat: get all user sidebar

--- backend/src/controller/userController.js
+++ backend/src/controller/userController.js
@@ -1,8 +1,16 @@
-
+const User = require('../models/userModel');
 
 class userController {
     async getUserForSidebar(req, res){
+        try{
+            const loggedInUserId = req.user._id;
+            const filteredUsers = await User.find({ _id: { $ne: loggedInUserId }} ).select("-password"); // Find all users except the logged in user
 
+            res.status(200).json(filteredUsers);
+        }catch (e){
+            console.log(e.message)
+            res.status(500).json({message: 'Error in userController getUserForSidebar'})
+        }
     }
 }
 
--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const userController = require('../controller/userController');
+const authMiddleware = require("../authMiddleware/authMiddleware");
 
 const router = express.Router();
 
-router.get('/', userController.getUserForSidebar);
+router.get('/',authMiddleware, userController.getUserForSidebar);
 
 module.exports = router;
