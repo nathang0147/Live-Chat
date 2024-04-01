db6a3fab46fb67e1ddb3716b732a1eb97798d44d
nathang0147 <nganhthang147@gmail.com>
2024-04-01 16:59:34 +0700

    feat: import userRouter

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,9 +1,8 @@
 const authRouter = require('./auth');
-const messagesRouter = require('./messages' +
-    '');
+const messagesRouter = require('./messages');
 function routes(app){
     app.use('/api/auth', authRouter);
-    app.use('/api/messages',)
+    app.use('/api/messages',messagesRouter)
     app.use('/', (req, res) => res.json({mssg: 'Home'}))
 }
 
