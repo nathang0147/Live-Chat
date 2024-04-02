6e43ab09fdd900416971aa63d020246b2e99180b
nathang0147 <nganhthang147@gmail.com>
2024-04-03 00:05:55 +0700

    feat: Home have sidebar and boxchat

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,7 +1,12 @@
+import Sidebar from "../../component/sidebar/Sidebar.jsx";
+import MessageContainer from "../../component/message/MessageContainer.jsx";
+
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-500 bg-clip-padding
+        backdrop-filter backdrop-blur-2xl bg-opacity-30">
+            <Sidebar/>
+            <MessageContainer/>
         </div>
     );
 };
