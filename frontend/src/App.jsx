37669571c18d0be53089ae65678ce2a34cf0152f
nathang0147 <nganhthang147@gmail.com>
2024-04-03 00:04:32 +0700

    fix: route of home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
             <Routes>
                 <Route path='/' element={<Login />} />
                 <Route path='/signup' element={<SignUp />} />
-                <Route path='/signup' element={<Home />} />
+                <Route path='/home' element={<Home />} />
             </Routes>
         </Router>
       </div>
