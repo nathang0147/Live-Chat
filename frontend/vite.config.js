f203c28a1bb38104d8d541aea35ee62a9465c75f
nathang0147 <nganhthang147@gmail.com>
2024-04-14 11:18:27 +0700

    fix: bug

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://live-chat-gray.vercel.app/',
+  server: {
+    host: "localhost",
+    port: 4000,
+  }
 })
